805f8a2202b4d93794be675793c927cffd1fcff5
Damirka1 <hamitov.damir3101@icloud.com>
2022-07-11 20:20:31 +0600

    Initial commit

---
+++ src/test/java/com/dam1rka/musicserver/MusicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam1rka.musicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
