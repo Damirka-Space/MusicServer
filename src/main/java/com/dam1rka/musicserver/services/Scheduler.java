423e04429830f60f8212998ce56923932409ec5d
Damirka1 <hamitov.damir3101@icloud.com>
2022-08-09 23:38:51 +0600

    Added scheduler, set up time for update playlists

---
+++ src/main/java/com/dam1rka/musicserver/services/Scheduler.java
@@ -0,0 +1,30 @@
+package com.dam1rka.musicserver.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduler {
+
+    private PlaylistService playlistService;
+
+    @Autowired
+    public Scheduler(PlaylistService playlistService) {
+        this.playlistService = playlistService;
+    }
+
+    // Run every day
+    @Scheduled(cron = "0 0 * * *")
+    public void updatePlaylistOfDay() {
+        playlistService.updatePlaylistOfDay();
+
+        playlistService.updateAllInOnePlaylist();
+    }
+
+    // Run every week
+    @Scheduled(cron = "0 0 * * 0")
+    public void updatePlaylistOfWeek() {
+        playlistService.updatePlaylistOfWeek();
+    }
+}
