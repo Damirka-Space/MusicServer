d53f0899e18028b5efe5e4591c8849bba807910b
Damirka1 <hamitov.damir3101@icloud.com>
2023-08-18 23:35:31 +0600

    Fixed select for liked albums

--- src/main/java/com/dam1rka/musicserver/repositories/AlbumRepository.java
+++ src/main/java/com/dam1rka/musicserver/repositories/AlbumRepository.java
@@ -13,7 +13,7 @@ public interface AlbumRepository extends JpaRepository<AlbumEntity, Long> {
 //    AlbumEntity findByTitle(String title);
     AlbumEntity findByTitleAndAuthorsIn(String title, List<AuthorEntity> authors);
 
-    @Query(value = "SELECT ae.id, ae.title, ae.description, ae.album_type_entity_id, ae.created, ae.updated, ae.image_id FROM liked_albums AS ls " +
+    @Query(value = "SELECT ae.id, ae.title, ae.description, ae.album_type_entity_id, ae.created, ae.updated, ae.image_id, ae.deleted FROM liked_albums AS ls " +
             "JOIN liked_albums_albums laa on ls.id = laa.liked_albums_id " +
             "JOIN album_entity ae on laa.albums_id = ae.id " +
             "WHERE ls.user_id = ?1 AND ae.album_type_entity_id = ?2 ORDER BY ae.created", nativeQuery = true)
